d74b1bf5d718da2894a205f6b16d10e11cfcf5fb
zhongcheng <zhongcheng@ichuangye.cn>
2017-09-15 15:04:22 +0800

    更新评论页面

--- src/filters/index.js
+++ src/filters/index.js
@@ -3,6 +3,19 @@ import moment from "moment";
 // 星期过滤器
 const week = (date)=>['星期天','星期一','星期二','星期三','星期四','星期五','星期六'][moment(date).format('d')];
 
+// 图片过滤器
+const attachImageUrl = (url) =>{
+    if (url !== undefined) {
+        return url.replace(/http\w{0,1}:\/\/p/g, 'https://images.weserv.nl/?url=p');
+    }
+}
+
+// 日期过滤器
+const commonDate = (date) => moment(date).format('YYYY-MM-DD hh:ss')
+
+
 export default {
-    week
+    week,
+    attachImageUrl,
+    commonDate
 }
--- src/pages/Comment.vue
+++ src/pages/Comment.vue
@@ -2,6 +2,31 @@
 @import '../assets/base.less';
 .comment{ 
   .comment-content{ height: 100%;padding-top: 40px;}
+  .comment-title{ 
+    height: @80px;line-height: @80px;border-bottom: 1px solid #e7e7e7;color: #333;padding: 0 @30px;
+    span{ font-size: @30px;}
+    a{ 
+      float: right;display: block;
+      transition: all 0.3s;    
+      &.active{ transform: rotate(180deg)}
+    }
+  }
+  .comment-body{
+    li{ 
+      padding: @30px;border-bottom: 1px solid #e7e7e7;position: relative;
+      img{ position: absolute;left: @30px;top: @30px;width: @80px;height: @80px;border-radius: 50%;}
+      .text{ 
+        margin-left: @100px;
+        h2{ 
+          overflow: hidden;margin-bottom: 5px;
+          span{ vertical-align: middle;font-size: @28px;}
+          a{ color: #666;float: right;}
+        }
+        div{ margin-bottom: @20px;line-height: @40px;font-size: @28px;color: #666;}
+        p{ font-size: @24px;color: #999;}
+      }
+    }
+  }
 }
 </style>
 
@@ -12,46 +37,81 @@
         <mt-button icon="back">返回</mt-button>
       </a>
     </mt-header>
-    <div class="comment-content" ref="comment_content">
+    <div class="comment-content">
       <div class="comment-box">
+        <!-- 长评 -->
         <div class="comment-long">
-          <div class="comment-title">12条长评论</div>
+          <div class="comment-title"><span>{{long.length}}条长评论</span></div>
           <div class="comment-body">
-            
+            <ul>
+              <li v-for="(item,index) in long" :key="index">
+                <img :src="attachImageUrl(item.avatar)" :alt="item.author">
+                <div class="text">
+                  <h2>
+                    <span>{{item.author}}</span>
+                    <a href="javascript:;"><i class="iconfont icon-zan"></i> {{item.likes}}</a>
+                  </h2>
+                  <div>{{item.content}}</div>
+                  <p>{{commonDate(item.time)}}</p>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-        <div class="comment-short"></div>
+        <!-- 短评 -->
+        <div class="comment-short" ref="comment_short">
+          <div class="comment-title">
+            <span>{{short.length}}条短评论</span>
+            <a href="javascript:;" :class="{active:shortShow}" @click="show"><i class="iconfont icon-zhankai"></i></a>
+          </div>
+          <div class="comment-body" v-show="shortShow">
+            <ul>
+              <li v-for="(item,index) in short" :key="index">
+                <img :src="attachImageUrl(item.avatar)" :alt="item.author">
+                <div class="text">
+                  <h2>
+                    <span>{{item.author}}</span>
+                    <a href="javascript:;"><i class="iconfont icon-zan"></i> {{item.likes}}</a>
+                  </h2>
+                  <div>{{item.content}}</div>
+                  <p>{{commonDate(item.time)}}</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
+    
   </div> 
 </template>
 
 <script>
 import axios from 'axios';
-import BScroll from 'better-scroll';
 import { mapGetters,mapActions } from 'vuex'
+import filters from '../filters'
+import Scroll from '../components/Bscroll/scroll';
 export default {
   name: 'comment',
   data() {
     return {
       long: [],
-      short: []
+      short: [],
+      shortShow:false
     }
   },
   computed: {
     ...mapGetters(['comments'])
   },
   components: {
-    
+    Scroll
   },
   created() {
-    this.$nextTick(() => {
-      this.scroll = new BScroll(this.$refs.comment_content, {
-        click: true
-      });
-    });
+    this.fetchLongData();
+    this.fetchShortData();
   },
   methods: {
+    ...filters,
     ...mapActions(['goBack']),
     // 获取长评数据
     fetchLongData() {
@@ -73,6 +133,11 @@ export default {
         console.log(error);
       });
     },
+    show(){
+      this.shortShow = !this.shortShow;
+      let h = this.$refs.comment_short.offsetTop;
+      let b = this.$refs.comment_short.offsetBottom;
+    }
   }
 };
 </script>
